34f943597ac8980aeab9031b5b1b8da826513327
Travis Craft <tlcraft@users.noreply.github.com>
2022-03-24 18:48:30 -0600

    Add routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UpdateGreetingComponent } from './update-greeting/update-greeting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'greeting', component: HelloWorldComponent},
+  { path: 'edit-greeting', component: UpdateGreetingComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
