392ffefe6244ca4ba9cb39eb4f049fa66a6085ac
Travis Craft <tlcraft@users.noreply.github.com>
2022-03-25 12:25:07 -0600

    Update routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UpdateGreetingComponent } from './update-greeting/update-greeting.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/greeting', pathMatch: 'full' },
   { path: 'greeting', component: HelloWorldComponent},
   { path: 'edit-greeting', component: UpdateGreetingComponent }];
 
