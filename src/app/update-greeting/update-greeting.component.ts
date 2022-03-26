801b1029ca73d9f8af64c7dd85d6333cbd66afc5
Travis Craft <tlcraft@users.noreply.github.com>
2022-03-26 13:01:37 -0600

    Add edit controls for greeting.

--- src/app/update-greeting/update-greeting.component.ts
+++ src/app/update-greeting/update-greeting.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { GreetingService } from '../services/greeting.service';
 
 @Component({
   selector: 'app-update-greeting',
@@ -6,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-greeting.component.scss']
 })
 export class UpdateGreetingComponent {
+  greetingForm = new FormControl("");
 
-  constructor() { }
+  constructor(private greetingService: GreetingService) { }
 
+  ngOnInit() {
+    const greeting = this.getCurrentGreeting();
+    this.greetingForm.setValue(greeting);
+  }
+
+  getCurrentGreeting(): string {
+    return this.greetingService.getGreeting();
+  }
+
+  saveGreeting(): void {
+    const newGreeting = this.greetingForm.value;
+    this.greetingService.setGreeting(newGreeting);
+  }
 }
